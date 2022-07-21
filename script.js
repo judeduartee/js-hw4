606535b97ef629366dd24f2f91e3064a7c0bca4b
sh <bbayshka@mail.ru>
2022-07-21 05:05:16 +0500

    ptoject

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+let name1 = prompt('Введите своё имя')
+let yearage = +prompt ('Введите свой год рождения')
+let yearnow = +prompt ('Введите нынешний год')
+
+alert( name1 + ',  '  + ' Ваш возраст: ' + (yearnow - yearage) )
